c38b8478ff5d077f5e81c276325af199a77585b2
Yash Singhal <yashgyy@gmail.com>
2018-12-06 01:21:51 +0530

    Added Files

--- Diffusion.py
+++ Diffusion.py
@@ -10,12 +10,17 @@ from ndlib.viz.bokeh.DiffusionPrevalence import DiffusionPrevalence
 import ndlib.models.epidemics.SIModel as si
 from ContinuousTime import ContinuousTime
 import sys
+import ndlib.models.opinions.VoterModel as vt
 import ndlib.models.epidemics.SISModel as sis
+import ndlib.models.epidemics.GeneralisedThresholdModel as gth
 import numpy
 from TimeAware import TimeAware
+from DynaDiff import DynaDiff
 vm = MultiPlot()
 
 g = nx.barabasi_albert_graph(1000,3)
+
+
 #When node v becomes active in step t, it is given a single chance to activate each currently inactive neighbor w; it succeeds with a probability p(v,w).
 #If w has multiple newly activated neighbors, their attempts are sequenced in an arbitrary order.
 #If v succeeds, then w will become active in step t + 1; but whether or not v succeeds, it cannot make any further attempts to activate w in subsequent rounds.
@@ -61,6 +66,37 @@ plot4=DiffusionPrevalence(Thr, trends).plot(width=400, height=400)
 vm.add_plot(plot3)
 vm.add_plot(plot4)
 
+#--------------------------------------------------------
+
+
+#he model is instantiated on a graph having a non-empty set of infected nodes.
+
+#The model is defined as follows:
+
+#At time t nodes become Infected with rate mu t/tau
+#Nodes for which the ratio of the active friends dropped below the threshold are moved to the Infected queue
+#Nodes in the Infected queue become infected with rate tau. If this happens check all its friends for threshold
+
+
+
+Generalised = gth.GeneralisedThresholdModel(g)
+config = mc.Configuration()
+config.add_model_parameter('percentage_infected', 0.1)
+config.add_model_parameter('tau', 5)
+config.add_model_parameter('mu', 5)
+
+threshold = 0.25
+for i in g.nodes():
+    config.add_node_configuration("threshold", i, threshold)
+
+Generalised.set_initial_status(config)
+iterations = model.iteration_bunch(10,0)
+print(Generalised.get_info(), iterations)
+trends=Generalised.build_trends(iterations)
+plotX=DiffusionTrend(Generalised,trends).plot(width=400,height=400)
+plotY=DiffusionPrevalence(Generalised,trends).plot(width=400,height=400)
+vm.add_plot(plotX)
+vm.add_plot(plotY)
 
 
 
@@ -97,6 +133,44 @@ plot8=DiffusionPrevalence(model, trends).plot(width=400, height=400)
 
 vm.add_plot(plot7)
 vm.add_plot(plot8)
+
+#The Voter model is one of the simplest models of opinion dynamics, originally introduced to analyse competition of species [1] and soon after applied to model elections [2].
+#The model assumes the opinion of an individual to be a discrete variable ±1.
+#The state of the population varies based on a very simple update rule: at each iteration, a random individual is selected, who then copies the opinion of one random neighbour.
+#Starting from any initial configuration, on a complete network the entire population converges to consensus on one of the two options. The probability that consensus is reached on opinion +1 is equal to the initial fraction of individuals holding that opinion [3].
+
+Voter = vt.VoterModel(g)
+config = mc.Configuration()
+config.add_model_parameter('percentage_infected', 0.1)
+
+Voter.set_initial_status(config)
+
+iterations=Voter.iteration_bunch(200)
+trends=Voter.build_trends(iterations)
+plotX1=DiffusionTrend(Voter,trends).plot(width=400,height=400)
+plotX2=DiffusionPrevalence(Voter,trends).plot(width=400,height=400)
+
+vm.add_plot(plotX1)
+vm.add_plot(plotX2)
+
+
+#The Sznajd model [1] is a variant of spin model employing the theory of social impact, which takes into account the fact that a group of individuals with the same opinion can influence their neighbours more than one single individual.
+#In the original model the social network is a 2-dimensional lattice, however we also implemented the variant on any complex networks.
+#Each agent has an opinion σi = ±1. At each time step, a pair of neighbouring agents is selected and, if their opinion coincides, all their neighbours take that opinion.
+#The model has been shown to converge to one of the two agreeing stationary states, depending on the initial density of up-spins (transition at 50% density).
+import ndlib.models.opinions.SznajdModel as sn
+
+Sznajd=sn.SznajdModel(g)
+config = mc.Configuration()
+config.add_model_parameter('percentage_infected', 0.1)
+Sznajd.set_initial_status(config)
+iterations=Sznajd.iteration_bunch(200)
+trends=Sznajd.build_trends(iterations)
+plotX3=DiffusionTrend(Sznajd,trends).plot(width=400,height=400)
+plotX4=DiffusionPrevalence(Voter,trends).plot(width=400,height=400)
+
+vm.add_plot(plotX3)
+vm.add_plot(plotX4)
 
 
 #---------------------------------------------------------------------------------
@@ -135,7 +209,7 @@ vm.add_plot(plot10)
 
 #--------------------------------------------------------------
 #Continuous time model
-Cont=TimeAware(g)
+Cont=DynaDiff(g)
 config=m.Configuration()
 config.add_model_parameter("percentage_infected",0.2)
 
